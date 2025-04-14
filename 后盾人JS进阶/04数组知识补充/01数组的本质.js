212d91097c61c45d22f8feaa1b1a6ff8a39a8840
Xiaozhang <2122151459@qq.com>
2025-04-14 23:15:08 +0800

    4.14.2

---
+++ 后盾人JS进阶/04数组知识补充/01数组的本质.js
@@ -0,0 +1,4 @@
+// 其实如果定义一个数组检查其数据类型会发现数组其实是一个对象 所以一些对象的方法对数组也是有效果的
+const arr=[1,2,3,4,5,5]
+console.log(typeof arr);
+console.table(arr)//可以看见表格
---
+++ 后盾人JS进阶/04数组知识补充/03创建数组细节.js
@@ -0,0 +1,4 @@
+let hd=[1]
+hd[4]='nihao'
+console.table(hd)
+console.log(hd[2],hd[3]);//会自动开辟空间不过数据是undefined
